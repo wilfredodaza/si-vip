cdea9b8249b054fcd22e1e05f00d17eaef563686
wilfredodaza <wilfredo.daza@gmail.com>
2023-06-17 23:40:06 -0500

    logo y favicon del  home

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -4,10 +4,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="nevao.net">
+    <meta name="description" content="Sistema de Información para ventans, inventarios y personal">
     <meta name="keywords" content="">
-    <meta name="author" content="IPLanetColomba S.A.S">
-    <title>Home  - Loono</title>
+    <meta name="author" content="IPlanetColomba S.A.S">
+    <title>SIVIP - IPlanet Colombia SAS</title>
     <link rel="stylesheet" type="text/css" href="<?= base_url('/css/vendors.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('/css/shepherd-theme-default.min.css') ?>">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -22,7 +22,7 @@
     <?php endif; ?>
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/css/vendors.min.css">
     <link rel="stylesheet" type="text/css" href="<?= base_url('/css/styles_iplanet.css') ?>">
-    <link rel="shortcut icon" href="<?= base_url('/assets/img/nevao.jpg') ?>" type="image/png">
+    <link rel="shortcut icon" href="<?= base_url('/assets/img/sivip.png') ?>" type="image/png">
     <link rel="stylesheet" href="<?= base_url('/assets/css/select2.min.css') ?>" type="text/css">
     <link rel="stylesheet" href="<?= base_url('/assets/css/select2-materialize.css') ?>" type="text/css">
     <link rel="stylesheet" type="text/css" href="<?= base_url('/assets/css/dashboard.css') ?>">
